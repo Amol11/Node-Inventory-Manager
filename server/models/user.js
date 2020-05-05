d1005b40bbdaa263d611e7452e05c411547a6930
Amol11 <amolsinghaniya@gmail.com>
2020-05-05 16:46:14 +0530

    added POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 mongoose.set('useCreateIndex', true);
 
@@ -80,6 +81,42 @@ UserSchema.statics.findByToken = function(token) {
         'tokens.access': 'auth',
     });
 };
+
+UserSchema.statics.findByCredentials = function(email, password) {
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if(!user){
+           return Promise.reject('No user found.');
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res){
+                    resolve(user);
+                }
+                else{
+                    reject('Passwords don\'t match.');
+                }
+            });
+        });
+    });
+};
+
+UserSchema.pre('save', function(next){
+    var user = this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            })
+        })
+    }
+    else{
+        next();
+    }
+});
 
 var User = mongoose.model('User', UserSchema);
 
